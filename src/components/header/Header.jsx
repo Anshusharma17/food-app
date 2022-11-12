f88684e59bb10f925a01119d7e35d12864eb8ca6
Satyam Tiwari <satyamtiwari9836@gmail.com>
2022-11-12 20:03:11 +0530

    12-11|20:02

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,8 +3,27 @@ import "./header.css"
 
 function Header() {
   return (
-    <div className='header-main'>Header</div>
-    //satyam
+    <div className='header-main'>
+      <div className='main'>
+
+      <div className='a'>icon</div>
+      <div className='b'>
+      <ul className='header-list'>
+        <li className='item-list'>
+          <a className='item-link' href="#">Home</a>
+          </li>
+        <li className='item-list'>
+          <a className='item-link' href="#">Menu</a>
+        </li>
+        <li className='item-list'>
+        <a className='item-link' href="#">About</a>
+        </li>
+      </ul>
+      </div>
+      
+      </div>
+     
+    </div>
   )
 }
 
